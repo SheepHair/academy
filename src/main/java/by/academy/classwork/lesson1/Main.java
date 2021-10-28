a696f649cb41082cd81952c59c4b568c30de78df
Vadim <wadim.sidoryk@mail.ru>
2021-10-28 19:35:22 +0300

    Надеюсь в последний раз

--- src/main/java/by/academy/classwork/lesson1/Main.java
+++ src/main/java/by/academy/classwork/lesson1/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Cat cat1 = new Cat(5);
         System.out.println(cat1);
-
+//
         Cat cat2 = new Cat(2, "Baki");
         System.out.println(cat2);
 
