7f788e6f31553035b26d34043ca0fbe5ed4aa79d
=test <xodko19@gmail.com>
2021-11-04 20:23:05 +0300

    Последний раз?

--- src/main/java/by/academy/classwork/lesson4/Namb2.java
+++ src/main/java/by/academy/classwork/lesson4/Namb2.java
@@ -8,7 +8,6 @@ public class Namb2 {
         System.out.println("Введите целое число: ");
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        //
         int sum = 0;
         for (int i = 0; i <= n; i++) {
             sum = sum + i;
