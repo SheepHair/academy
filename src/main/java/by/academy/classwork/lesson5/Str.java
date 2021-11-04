c474f6ccd2a713e6e682b3f71166c5b313e0f39a
=test <xodko19@gmail.com>
2021-11-04 21:24:25 +0300

    задания с пятого задания

---
+++ src/main/java/by/academy/classwork/lesson5/Str.java
@@ -0,0 +1,24 @@
+package by.academy.classwork.lesson5;
+
+import java.util.Scanner;
+
+public class Str {
+    public static void main(String[] args) {
+        System.out.println("Введите количество строк:");
+        Scanner coll = new Scanner(System.in);
+        int n = coll.nextInt();
+        int i = 0;
+
+        String minStr = "";
+        for(int i = 1; i <= n ; i++)
+        {
+            System.out.println("Введите строку: ");
+            Scanner string = new Scanner(System.in);
+            String str = st()
+                    if (minStr; lenght > str.lenght)
+        }
+
+        String string = new String();
+        System.out.println(string);
+    }
+}
