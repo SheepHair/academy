6eec0a4a625e32f43c62dab36730d848701da117
=test <xodko19@gmail.com>
2021-11-11 21:24:59 +0300

    Классы

---
+++ src/main/java/by/academy/classwork/lesson7/Boat.java
@@ -0,0 +1,11 @@
+package by.academy.classwork.lesson7;
+
+public class Boat extends Product {
+    public Boat() {
+        super();
+        this.type = "Boat";
+        public Boat (double price, String name) {
+            super(price, "Boat",);
+        }
+    }
+}
--- src/main/java/by/academy/classwork/lesson7/Car.java
+++ src/main/java/by/academy/classwork/lesson7/Car.java
@@ -4,5 +4,9 @@ public class Car extends Product{
     public Car(){
         super();
         this.type = "Car";
+
+        public Car (double price, String name) {
+            super(price, "Car",);
+        }
     }
 }
