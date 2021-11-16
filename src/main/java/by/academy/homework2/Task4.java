686238657b4c794de8c639806dc74e8861dd34c8
SheepHair <xodko19@gmail.com>
2021-11-16 21:26:24 +0300

    4

--- src/main/java/by/academy/homework2/Task4.java
+++ src/main/java/by/academy/homework2/Task4.java
@@ -1,4 +1,34 @@
 package by.academy.homework2;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task4 {
+    public static void main(String[] args) {
+
+        int numberofplayers;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите количество игроков: ");
+        numberofplayers = sc.nextInt();
+
+        String[] cards = {"Червь", "Бубен", "Пика", "Треф"};
+        String[] suit = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз"};
+
+        Random rand = new Random();
+
+        for (int i = 1; i <= numberofplayers; i++) {
+            System.out.println("");
+            System.out.print("Игрок №" + i+ ":");
+            for (int a = 0; a < 5; a++) {
+                String card1 = cards[rand.nextInt(cards.length)];
+                System.out.print(Arrays.toString(new String[]{card1}));
+
+                String card2 = suit[rand.nextInt(suit.length)];
+                System.out.print(Arrays.toString(new String[]{card2}));
+                System.out.print(" ");
+            }
+        }
+    }
 }
