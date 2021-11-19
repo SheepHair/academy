1bcf5cbb0ed301293c30c7817a5d5fca32ecece5
SheepHair <xodko19@gmail.com>
2021-11-20 01:22:48 +0300

    4

--- src/main/java/by/academy/homework2/Task4.java
+++ src/main/java/by/academy/homework2/Task4.java
@@ -40,17 +40,3 @@ public class Task4 {
         scan.close();
     }
 }
-   /*     for (int i = 1; i <= numberofplayers; i++) {
-            System.out.println(";");
-            System.out.print("Игрок №" + i+ ":");
-            for (int a = 0; a < 5; a++) {
-                String card1 = cards[rand.nextInt(cards.length)];
-                System.out.print(Arrays.toString(new String[]{card1}));
-
-                String card2 = suit[rand.nextInt(suit.length)];
-                System.out.print(Arrays.toString(new String[]{card2}));
-                System.out.print(" ");
-            }
-        }*/
-
-
